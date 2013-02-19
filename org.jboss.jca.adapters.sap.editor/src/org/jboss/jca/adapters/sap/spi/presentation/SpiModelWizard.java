f3eaf5da0a65943adf88de7ff4499277bc6bd3ba
William Collins <wcollins@redhat.com>
2013-02-19 15:41:05 -0500

    Updated plugin meta data files

--- org.jboss.jca.adapters.sap.editor/src/org/jboss/jca/adapters/sap/spi/presentation/SpiModelWizard.java
+++ org.jboss.jca.adapters.sap.editor/src/org/jboss/jca/adapters/sap/spi/presentation/SpiModelWizard.java
@@ -419,8 +419,7 @@ public class SpiModelWizard extends Wizard implements INewWizard {
 		 * @generated
 		 */
 		public void createControl(Composite parent) {
-			Composite composite = new Composite(parent, SWT.NONE);
-			{
+			Composite composite = new Composite(parent, SWT.NONE); {
 				GridLayout layout = new GridLayout();
 				layout.numColumns = 1;
 				layout.verticalSpacing = 12;
